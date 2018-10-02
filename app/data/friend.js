4894056b4f21b403168a9457bf6db66b7e9abb50
Donnie Toombs <donnietoombs@gmail.com>
2018-10-02 01:28:02 -0400

    updating friends

--- app/data/friend.js
+++ app/data/friend.js
@@ -2,7 +2,7 @@
 var friends =
 [
     {
-    name: "Ahmed",
+    name: "Bruce Wayne",
     photo: "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
     scores: [
     5,
@@ -19,7 +19,7 @@ var friends =
     },
     
     {
-    name: "Jacob Deming",
+    name: "Clark Kent",
     photo: "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
     scores: [
     4,
@@ -36,7 +36,7 @@ var friends =
     },
     
     {
-    name: "Jeremiah Scanlon",
+    name: "Wade Wilson",
     photo: "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
     scores: [
     5,
@@ -53,7 +53,7 @@ var friends =
     },
     
     {
-    name: "Louis T. Delia",
+    name: "Bruce Banner",
     photo: "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
     scores: [
     3,
@@ -70,7 +70,7 @@ var friends =
     },
     
     {
-    name: "Lou Ritter",
+    name: "Tony Stark",
     photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAkDAAAAJDhhZTI5NTk2LWQzZjUtNDJjZi1hMTM2LTQ3ZjNmYjE0YmY2NA.jpg",
     scores: [
     4,
@@ -87,7 +87,7 @@ var friends =
     },
     
     {
-    name: "Jordan Biason",
+    name: "Peter Parker",
     photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAisAAAAJGUxYzc4YzA0LWQxMzUtNGI4NS04YTFiLTkwYzM0YTZkNzA2NA.jpg",
     scores: [
     4,
@@ -104,7 +104,7 @@ var friends =
     },
     
     {
-    name: "Jake Green",
+    name: "Hal Jordan",
     photo: "https://www.google.ca/search?q=charlie+chaplin&rlz=1C1CHBF_enCA800CA800&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjD2qeK_OLdAhUD4IMKHXDeAOQQ_AUIDigB&biw=1366&bih=664#",
     scores: [
     4,
@@ -121,7 +121,7 @@ var friends =
     },
     
     {
-    name: "George Bush",
+    name: "Selina Kyle",
     photo: "https://upload.wikimedia.org/wikipedia/commons/d/d4/George-W-Bush.jpeg",
     scores: [
     5,
@@ -172,7 +172,7 @@ var friends =
     },
     
     {
-    name: "GMAN",
+    name: "Jean Grey",
     photo: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fvignette.wikia.nocookie.net%2Fhalf-life%2Fimages%2F2%2F27%2FG-man_heart_to_heart_headshot.jpg%2Frevision%2Flatest%3Fcb%3D20100621095818%26path-prefix%3Den&imgrefurl=http%3A%2F%2Fhalf-life.wikia.com%2Fwiki%2FThe_G-Man&docid=La8CBZcf_NhFbM&tbnid=CdOZemu654UdlM%3A&vet=10ahUKEwiXqpzEzOPdAhVnuVkKHdrqDcEQMwh8KAAwAA..i&w=579&h=575&safe=active&bih=648&biw=1081&q=gman&ved=0ahUKEwiXqpzEzOPdAhVnuVkKHdrqDcEQMwh8KAAwAA&iact=mrc&uact=8",
     scores: [
     2,
