2be9406da20eca5644de2aa2b90675e3eb852680
Donnie Toombs <donnietoombs@gmail.com>
2018-10-02 01:21:49 -0400

    updating app

---
+++ app/data/friend.js
@@ -0,0 +1,226 @@
+// Data array that will store the friends data
+var friends =
+[
+    {
+    name: "Ahmed",
+    photo: "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+    scores: [
+    5,
+    1,
+    4,
+    4,
+    5,
+    1,
+    2,
+    5,
+    4,
+    1
+    ]
+    },
+    
+    {
+    name: "Jacob Deming",
+    photo: "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+    scores: [
+    4,
+    2,
+    5,
+    1,
+    3,
+    2,
+    2,
+    1,
+    3,
+    2
+    ]
+    },
+    
+    {
+    name: "Jeremiah Scanlon",
+    photo: "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+    scores: [
+    5,
+    2,
+    2,
+    2,
+    4,
+    1,
+    3,
+    2,
+    5,
+    5
+    ]
+    },
+    
+    {
+    name: "Louis T. Delia",
+    photo: "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+    scores: [
+    3,
+    3,
+    4,
+    2,
+    2,
+    1,
+    3,
+    2,
+    2,
+    3
+    ]
+    },
+    
+    {
+    name: "Lou Ritter",
+    photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAkDAAAAJDhhZTI5NTk2LWQzZjUtNDJjZi1hMTM2LTQ3ZjNmYjE0YmY2NA.jpg",
+    scores: [
+    4,
+    3,
+    4,
+    1,
+    5,
+    2,
+    5,
+    3,
+    1,
+    4
+    ]
+    },
+    
+    {
+    name: "Jordan Biason",
+    photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAisAAAAJGUxYzc4YzA0LWQxMzUtNGI4NS04YTFiLTkwYzM0YTZkNzA2NA.jpg",
+    scores: [
+    4,
+    4,
+    2,
+    3,
+    2,
+    2,
+    3,
+    2,
+    4,
+    5
+    ]
+    },
+    
+    {
+    name: "Jake Green",
+    photo: "https://www.google.ca/search?q=charlie+chaplin&rlz=1C1CHBF_enCA800CA800&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjD2qeK_OLdAhUD4IMKHXDeAOQQ_AUIDigB&biw=1366&bih=664#",
+    scores: [
+    4,
+    5,
+    5,
+    2,
+    5,
+    3,
+    5,
+    1,
+    1,
+    4
+    ]
+    },
+    
+    {
+    name: "George Bush",
+    photo: "https://upload.wikimedia.org/wikipedia/commons/d/d4/George-W-Bush.jpeg",
+    scores: [
+    5,
+    2,
+    3,
+    3,
+    3,
+    4,
+    3,
+    3,
+    3,
+    2
+    ]
+    },
+    
+    {
+    name: "George Bush Jr.",
+    photo: "https://upload.wikimedia.org/wikipedia/commons/d/d4/George-W-Bush.jpeg",
+    scores: [
+    5,
+    2,
+    3,
+    3,
+    3,
+    4,
+    3,
+    3,
+    3,
+    2
+    ]
+    },
+    
+    {
+    name: "luke",
+    photo: "Luke-rotjpromo.jpg",
+    scores: [
+    4,
+    3,
+    3,
+    4,
+    3,
+    2,
+    5,
+    2,
+    2,
+    2
+    ]
+    },
+    
+    {
+    name: "GMAN",
+    photo: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fvignette.wikia.nocookie.net%2Fhalf-life%2Fimages%2F2%2F27%2FG-man_heart_to_heart_headshot.jpg%2Frevision%2Flatest%3Fcb%3D20100621095818%26path-prefix%3Den&imgrefurl=http%3A%2F%2Fhalf-life.wikia.com%2Fwiki%2FThe_G-Man&docid=La8CBZcf_NhFbM&tbnid=CdOZemu654UdlM%3A&vet=10ahUKEwiXqpzEzOPdAhVnuVkKHdrqDcEQMwh8KAAwAA..i&w=579&h=575&safe=active&bih=648&biw=1081&q=gman&ved=0ahUKEwiXqpzEzOPdAhVnuVkKHdrqDcEQMwh8KAAwAA&iact=mrc&uact=8",
+    scores: [
+    2,
+    3,
+    2,
+    1,
+    2,
+    4,
+    4,
+    4,
+    2,
+    2
+    ]
+    },
+    
+    {
+    name: "ben",
+    photo: "www.myspace.com/hello",
+    scores: [
+    5,
+    2,
+    3,
+    3,
+    1,
+    5,
+    1,
+    5,
+    1,
+    4
+    ]
+    },
+
+    {
+    name: "Natasha",
+    photo: "https://github.com/afhaque/friendfinder",
+    scores: [
+    2,
+    3,
+    3,
+    2,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1
+    ]
+    }
+];
+
+    module.exports = friends;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,29 @@
+var friends = require("../data/friend.js");
+
+
+module.exports = function(app) {
+
+    app.get("/api/friends", function(req, res) {
+    res.json(friends);
+    });
+
+
+    app.post("/api/friends", function(req, res) {
+        
+        
+        var newFriend = req.body
+    
+        console.log(newFriend);
+
+        friends.push(newFriend);
+
+        res.json(newFriend);
+  
+});
+
+
+}
+
+
+
+
